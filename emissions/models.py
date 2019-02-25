45b0b7ca962d470dd0c4851f560c38b5f2ddd449
Kaarlo Kekkonen <kaarlo.kekkonen@aalto.fi>
2019-02-25 12:53:38 +0200

    fixed country name field length

--- emissions/models.py
+++ emissions/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Country_population(models.Model):
-	Country_Name = models.CharField(max_length=30)
+	Country_Name = models.CharField(max_length=50)
 	Country_Code = models.CharField(max_length=5)
 	Indicator_Name = models.CharField(max_length=50)
-	Indicator_Code = models.CharField(max_length=51)
+	Indicator_Code = models.CharField(max_length=50)
 	population_values = models.TextField()
 
 class Country_emissions(models.Model):
-	Country_Name = models.CharField(max_length=31)
-	Country_Code = models.CharField(max_length=4)
-	Indicator_Name = models.CharField(max_length=52)
-	Indicator_Code = models.CharField(max_length=53)
+	Country_Name = models.CharField(max_length=50)
+	Country_Code = models.CharField(max_length=5)
+	Indicator_Name = models.CharField(max_length=50)
+	Indicator_Code = models.CharField(max_length=50)
 	emission_values = models.TextField()
